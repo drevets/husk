7f4d508dcd676aa470a6bf67b17b3a37fae289fc
Emily Drevets-Larson <drevets@Emilys-MacBook-Pro-2.local>
2022-08-15 18:31:28 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # husk
-A husk of a project 
+
+This is a simple repository with a React (Typescript) frontend and a NestJS (Typescript) backend connected to a postgres database.
+
+Before running the project, you need to make sure you have Docker and yarn installed.
+
+# Running the project
+
+1. Run `docker-compose up` in the root of the directory to start the database.
+2. `cd` into `/backend` and run `yarn` to install dependencies. Then run `yarn start:dev` to start the backend in live reload mode.
+3. `cd` into `/frontend` and run `yarn` to install dependencies. Then run `yarn start` to start the frontend.
+
+# More info
+
+To learn more about Create React App, you can visit (this link)[https://create-react-app.dev/].
+Learn more about NestJS at (nestjs.com)[https://nestjs.com/].
---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'husk',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
